8e7bff9c832175bae32596bd274dac38758c3a33
Danritchell <dannyneverfade@yahoo.com>
2020-01-14 02:08:13 +0800

    added custom fonts & script to generate image require

---
+++ assets/generate-image.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const { readdir, writeFileSync } = require('fs');
+const { basename, extname, join } = require('path');
+
+readdir(join(__dirname, 'images'), (err, files) => {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  const mapped = files
+    .filter(file => file.indexOf('@') < 0)
+    .reduce((result, file) => result.concat(imageAssociation(file)), [])
+    .join(';\n');
+
+  const content = `
+import { Image } from 'react-native';
+${mapped}
+	`;
+
+  writeFileSync(join(__dirname, 'images.js'), content);
+});
+
+function titleCase(string) {
+  return string
+    .split(/[-|_]/)
+    .filter(string => string)
+    .map(string => string[0].toUpperCase() + string.substr(1).toLowerCase())
+    .join('');
+}
+
+function imageAssociation(file) {
+  const key = titleCase(basename(file, extname(file)));
+  return `export const ${key} = require('./images/${basename(file)}')`;
+}
