1f26597de4ec497827ed090b2564df5f4f6b0dc7
Danritchell <dannyneverfade@yahoo.com>
2020-01-14 02:14:24 +0800

    transfer App.js to src folder

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+if (__DEV__) {
+  XMLHttpRequest = GLOBAL.originalXMLHttpRequest
+    ? GLOBAL.originalXMLHttpRequest
+    : GLOBAL.XMLHttpRequest;
+}
 
 AppRegistry.registerComponent(appName, () => App);
